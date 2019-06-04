c4560e0c943de37e12badfaf486e50ebc8d2d70c
benjamin9625 <sipos.benjamin96@gmail.com>
2019-06-04 13:49:00 +0200

    first commit

---
+++ appvars.php
@@ -0,0 +1,8 @@
+<?php
+  // Define application constants
+  define('MM_UPLOADPATH', 'images/');
+  define('PH_UPLOADPATH', 'C:\inetpub\wwwroot\Web\mismatch\images\\');
+  define('MM_MAXFILESIZE', 32768);      // 32 KB
+  define('MM_MAXIMGWIDTH', 120);        // 120 pixels
+  define('MM_MAXIMGHEIGHT', 120);       // 120 pixels
+?>
